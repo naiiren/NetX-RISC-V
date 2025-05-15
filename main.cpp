1e6b1acefbf30f2aab259bc22efd09e4bbc67a3b
nairen <nairen@smail.nju.edu.cn>
2025-05-15 20:33:21 +0800

    update unit tests

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <fstream>
 #include <filesystem>
+#include <format>
 #include <nxsim/circuit.h>
 #include <nxsim/circuit_parser.h>
 
+// #define DEBUG
+
 using namespace nxon;
+
+const static auto magic_instr = value_t{32, 0xdead10cc};
+
+auto high = value_t{1, 1};
+auto low = value_t{1, 0};
 
 unsigned singed_extend(const unsigned size, const unsigned value) {
     if (value & 1 << size) {
@@ -13,18 +21,22 @@ unsigned singed_extend(const unsigned size, const unsigned value) {
 }
 
 class Memory {
+    static constexpr unsigned MEMORY_SIZE = 32768;
+
     std::vector<std::byte> memory;
 
-    [[nodiscard]] unsigned read_byte(const unsigned addr, const unsigned offset) const {
+    [[nodiscard]] unsigned read_bytes(unsigned addr, const unsigned offset) const {
+        addr &= MEMORY_SIZE - 1;
         unsigned result = 0;
         for (int i = 0; i != offset; ++i) {
             result <<= 8;
-            result |= static_cast<unsigned>(memory[addr + i]);
+            result |= static_cast<unsigned>(memory[addr + offset - i - 1]);
         }
         return result;
     }
 
-    void write_byte(const unsigned addr, unsigned value) {
+    void write_word(unsigned addr, unsigned value) {
+        addr &= MEMORY_SIZE - 1;
         for (int i = 0; i != 4; ++i) {
             memory[addr + 3 - i] = static_cast<std::byte>(value & 0xFF);
             value >>= 8;
@@ -33,87 +45,148 @@ class Memory {
 
 public:
     explicit Memory(std::ifstream fin) {
+        memory.resize(MEMORY_SIZE);
+
         std::string line;
+        unsigned addr = 0;
         while (std::getline(fin, line)) {
             if (line.empty()) continue;
-            unsigned value = std::stoul(line, nullptr, 16);
-            for (int i = 0; i < 4; ++i) {
-                memory.push_back(static_cast<std::byte>(value & 0xFF));
-                value >>= 8;
+            if (line[0] == '@') {
+                addr = std::stoul(line.substr(1));
+            } else {
+                unsigned value = std::stoul(line, nullptr, 16);
+                for (int i = 0; i < 4; ++i) {
+                    memory[addr++] = static_cast<std::byte>(value & 0xFF);
+                    value >>= 8;
+                }
             }
         }
     }
 
-    [[nodiscard]] value_t read32(const unsigned addr) const {
-        return {32, read_byte(addr, 4)};
+    [[nodiscard]] value_t read_word(const unsigned addr) const {
+        return {32, read_bytes(addr, 4)};
+    }
+
+    [[nodiscard]] value_t read_word(const value_t &addr) const {
+        return read_word(static_cast<unsigned>(addr));
     }
 
     [[nodiscard]] value_t read_with_op(const value_t &memOP, const unsigned addr) const {
-        switch (memOP) {
-            case 0b000 : return {32, singed_extend(8, read_byte(addr, 1))};
-            case 0b001 : return {32, singed_extend(16, read_byte(addr, 2))};
-            case 0b010 : return {32, read_byte(addr, 4)};
-            case 0b101 : return {32, read_byte(addr, 2)};
-            case 0b100 : return {32, read_byte(addr, 1)};
-            default : assert(false);
+        switch (static_cast<unsigned>(memOP)) {
+            case 0b000u : return {32, singed_extend(8, read_bytes(addr, 1))};
+            case 0b001u : return {32, singed_extend(16, read_bytes(addr, 2))};
+            case 0b010u : return {32, read_bytes(addr, 4)};
+            case 0b101u : return {32, read_bytes(addr, 2)};
+            case 0b100u : return {32, read_bytes(addr, 1)};
+            default : {
+                std::cout << "Invalid read memory operation: " << static_cast<unsigned>(memOP) << std::endl;
+                std::abort();
+            }
         }
     }
 
+    [[nodiscard]] value_t read_with_op(const value_t &memOP, const value_t &addr) const {
+        return read_with_op(memOP, static_cast<unsigned>(addr));
+    }
+
     void write_with_op(const value_t &memOP, const unsigned addr, const value_t &value) {
-        switch (memOP) {
-            case 0b000 : write_byte(addr, singed_extend(8, static_cast<unsigned>(value) & 0xFF));
-            case 0b001 : write_byte(addr, singed_extend(16, static_cast<unsigned>(value) & 0xFFFF));
-            case 0b010 : write_byte(addr, static_cast<unsigned>(value));
-            case 0b101 : write_byte(addr, static_cast<unsigned>(value) & 0xFFFF);
-            case 0b100 : write_byte(addr, static_cast<unsigned>(value) & 0xFF);
-            default : assert(false);
+        switch (static_cast<unsigned>(memOP)) {
+            case 0b000u : write_word(addr, singed_extend(8, static_cast<unsigned>(value) & 0xFF)); break;
+            case 0b001u : write_word(addr, singed_extend(16, static_cast<unsigned>(value) & 0xFFFF)); break;
+            case 0b010u : write_word(addr, static_cast<unsigned>(value)); break;
+            case 0b101u : write_word(addr, static_cast<unsigned>(value) & 0xFFFF); break;
+            case 0b100u : write_word(addr, static_cast<unsigned>(value) & 0xFF); break;
+            default : {
+                std::cout << "Invalid write memory operation: " << static_cast<unsigned>(memOP) << std::endl;
+                std::abort();
+            }
         }
     }
+
+    void write_with_op(const value_t &memOP, const value_t &addr, const value_t &value) {
+        write_with_op(memOP, static_cast<unsigned>(addr), value);
+    }
 };
+
+void print(const std::string &head, const value_t &value) {
+    std::cout << head << std::setw(8) << std::setfill('0') << std::hex << static_cast<unsigned>(value) << '\t';
+}
 
 int main() {
     std::string json;
     std::getline(std::cin, json);
     auto ctx = parse_circuit(nlohmann::json::parse(json));
 
-    auto time_advance = [&]() {
-        ctx.flip_by_name("clk");
-        ctx.flip_by_name("clk");
-    };
-
-    std::filesystem::path path = std::filesystem::current_path().append("testcases");
-    for (const auto& entry : std::filesystem::directory_iterator(path)) {
+    int passed = 0, total = 0;
+    for (std::filesystem::path path = std::filesystem::current_path().append("testcases");
+        const auto& entry : std::filesystem::directory_iterator(path)) {
         std::filesystem::path file_path = entry.path();
+
+#ifdef DEBUG
+        if (file_path.filename() != "blt.hex") {
+            continue;
+        }
+#endif
+
         if (file_path.extension() == ".data") {
             continue;
         }
-        
-        std::cout << "================="
-                  << " Running test case: " << file_path.filename()
-                  << " ================"
-                  << std::endl;
 
+        if (file_path.filename() == "fence_i.hex") {
+            continue;
+        }
+
+        total++;
+        std::cout << "Running test case: " << file_path.filename();
+
+#ifdef DEBUG
+        std::cout << std::endl;
+#endif
         const auto instr_mem = new Memory(std::ifstream(file_path));
         const auto data_mem = new Memory(std::ifstream(file_path.replace_extension(".data")));
 
-        ctx.update_by_name("rst", value_t{1, 1}); time_advance();
+        ctx.flip_by_name("clk");
+        ctx.update_by_name("rst", value_t{1, 1});
+        ctx.flip_by_name("clk");
+        ctx.flip_by_name("clk");
         ctx.update_by_name("rst", value_t{1, 0});
 
-        const auto i_addr = static_cast<unsigned>(ctx.get_by_name("imemAddr"));
-        const auto d_addr = static_cast<unsigned>(ctx.get_by_name("dmemAddr"));
+        for (int i = 0 ; i != 1000; ++i) {
+            const auto instr = instr_mem->read_word(ctx.get_by_name("imemAddr"));
+            ctx.flip_by_name("clk");
 
-        const static auto magic_instr = value_t{32, 0xdead10cc};
-        if (const auto next_instr = instr_mem->read32(i_addr); next_instr == magic_instr) {
-            std::cout << "End of Program" << std::endl;
-        } else {
-            ctx.update_by_name("imemDataOut", next_instr);
+            ctx.update_by_name("imemDataOut", instr);
 
+            auto d_mem_op = ctx.get_by_name("dmemOp");
+            auto d_mem_addr = ctx.get_by_name("dmemAddr");
+            if (ctx.get_by_name("dmemWe") == high) {
+                auto d_mem_in = ctx.get_by_name("dmemDataIn");
+                data_mem->write_with_op(d_mem_op, d_mem_addr, d_mem_in);
+            }
+
+            // print("instr: ", instr);
+            // for (int j = 1; j < 4; ++j) {
+            //     print(std::format("x{}: ", j), ctx.get_by_name(std::format("data[{}]", j)));
+            // }
+            // print("a0: ", ctx.get_by_name(std::format("data[10]")));
+            // std::cout << std::endl;
+
+            if (instr == magic_instr) {
+                if (static_cast<unsigned>(ctx.get_by_name("data[10]")) == 0x00c0ffee) {
+                    std::cout << "\t-> \033[32mPassed!\033[0m" << std::endl;
+                    passed++;
+                } else {
+                    std::cout << "\t-> \033[31mFailed!\033[0m" << std::endl;
+                }
+                break;
+            }
+
+            ctx.flip_by_name("clk");
+            ctx.update_by_name("dmemDataOut", data_mem->read_with_op(d_mem_op, d_mem_addr));
         }
-
         delete instr_mem;
         delete data_mem;
-        break;
     }
-
+    std::cout << std::format("Passed {}/{} test cases\n", passed, total);
     return 0;
 }
